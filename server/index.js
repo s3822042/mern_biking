34869e207a385cc6fc758ee5bfd44306025e0368
s3822042 <s3822042@rmit.edu.vn>
2021-07-14 17:13:10 +0700

    add authentication

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRouter = require("./routes/auth.js");
+const authRouter = require("./routes/auth");
 
 const connectDB = async () => {
   try {
