74f98cab1da5fd8dbfe661d7a45f2f67cb601aff
s3822042 <s3822042@rmit.edu.vn>
2021-07-18 19:24:00 +0700

    add api caller

---
+++ client/src/app/utils/apiCaller.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default function apiCaller(url: string) {
+    return axios
+        .get(url)
+        .then((res) => res.data)
+        .catch((error) => {
+            if (process.env.NODE_ENV === "development") {
+                console.error(error);
+            }
+        });
+}
+
+export async function bikingSearchData() {
+    const fetchUrl = ``;
+    const items = await apiCaller(fetchUrl);
+    return { fetchUrl, value: items };
+}
+
+export async function bikingFetchData() {
+    const fetchUrl = ``;
+    const items = await apiCaller(fetchUrl);
+    return { fetchUrl, value: items };
+}
